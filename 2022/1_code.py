a84918451f8274707fb09b863965f273f6d55c77
Johnny Bravo <HaKmonkey@gmail.com>
2022-12-11 05:07:29 -0500

    Update 1_code.py
    Simplified using list comprehensions instead of pandas because the only info I needed was the calories

--- 2022/1_code.py
+++ 2022/1_code.py
@@ -1,21 +1,11 @@
-import pandas as pd
+def main():
+    with open('1_input.txt') as f:
+        data = ','.join([line.strip() for line in f]).split(',,')
+        data = [x.split(',') for x in data]
+        f = lambda x: sum([int(cal) for cal in x])
+        results = sorted([f(x) for x in data])
+        print(f'SILVER: {results[-1]}')
+        print(f'GOLD: {sum(results[-3:])}')
 
-with open('1_input.txt') as f:
-    count = 1
-    total = 0
-    df = pd.DataFrame(columns=['Elf', 'Calories'])
-    for line in f:
-        if line != '\n':
-            total += int(line)
-        else:
-            df.loc[count-1] = [count, total]
-            total = 0
-            count += 1
-    df.loc[count-1] = [count, total]
-    top_elf = df.nlargest(1, ['Calories']).to_string(index=False)
-    print(f'The elf with the most calories is:\n\n{top_elf}')
-    top_three = df.nlargest(3, ['Calories']).to_string(index=False)
-    print(f'The top three elves are:\n\n{top_three}')
-    top_three_sum = df.nlargest(3, ['Calories']).sum()
-    top_three_cals = top_three_sum['Calories']
-    print(f'Total cals of top three: {top_three_cals}')
+if __name__ == '__main__':
+    main()
