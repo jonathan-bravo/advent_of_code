1e89b2344aee0f5ac9d3d972dfeaaa7974b8c21a
Johnny Bravo <HaKmonkey@gmail.com>
2022-12-11 05:57:55 -0500

    Update 4_code.py
    generalized the simulation from silver and gold into one function

--- 2022/4_code.py
+++ 2022/4_code.py
@@ -1,28 +1,24 @@
-def gold(pair):
+def simulate(pair, metal): # gold
     pair = pair.strip().split(',')
     [a,b] = pair[0].split('-')
     [x,y] = pair[1].split('-')
     r1 = range(int(a),int(b)+1)
     r2 = range(int(x),int(y)+1)
-    if set(r1).intersection(set(r2)) or set(r2).intersection(set(r1)):
+    cases = {
+        'SILVER': set(r1).issubset(set(r2)) or set(r2).issubset(set(r1)),
+        'GOLD': set(r1).intersection(set(r2)) or set(r2).intersection(set(r1))}
+    if cases[metal]:
         return 1
     else:
         return 0
 
-def silver(pair):
-    pair = pair.strip().split(',')
-    [a,b] = pair[0].split('-')
-    [x,y] = pair[1].split('-')
-    r1 = range(int(a),int(b)+1)
-    r2 = range(int(x),int(y)+1)
-    if set(r1).issubset(set(r2)) or set(r2).issubset(set(r1)):
-        return 1
-    else:
-        return 0
+def main():
+    with open('4_input.txt') as f:
+        pairs = f.readlines()
+        silver = sum([simulate(pair, 'SILVER') for pair in pairs])
+        gold = sum([simulate(pair, 'GOLD') for pair in pairs])
+        print(f'SILVER: {silver}')
+        print(f'GOLD: {gold}')
 
-with open('4_input.txt') as f:
-    pairs = f.readlines()
-    num_overlaps_silver = sum(map(silver, pairs))
-    print(num_overlaps_silver)
-    num_overlaps_gold = sum(map(gold, pairs))
-    print(num_overlaps_gold)
+if __name__ == '__main__':
+    main()
