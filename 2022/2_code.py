64442d42f6bd8a064a47871cd023f3d774f9d1ce
Johnny Bravo <HaKmonkey@gmail.com>
2022-12-11 05:42:43 -0500

    Update 2_code.py
    generalized the simulation function

--- 2022/2_code.py
+++ 2022/2_code.py
@@ -1,32 +1,25 @@
-def parse_round_gold(val):
-    val_dict = {'A':0, 'B':1, 'C':2}
-    them = val_dict[val[0]]
-    me = val[2]
-    if me == 'Y':
-        score = them + 4
-    elif me == 'Z':
-        score = ((them + 1) % 3) + 7
-    elif me == 'X':    
-        score = ((them - 1) % 3) + 1
-    return score
-
-
-def parse_round_silver(val):
-    val_dict = {'A':1, 'X':1, 'B':2, 'Y':2, 'C':3, 'Z':3}
+def simulate(val, metal):
+    val_dict = {'A':0, 'X':0, 'B':1, 'Y':1, 'C':2, 'Z':2}
     them = val_dict[val[0]]
     me = val_dict[val[2]]
-    if me == them:
-        score = me + 3
-    elif (me-them)%3 == 1:
-        score = me + 6
-    elif (me-them)%3 == 2:
-        score = me
+    if metal == 'SILVER':
+        if me == them: score = me + 4
+        elif (me-them)%3 == 1: score = me + 7
+        elif (me-them)%3 == 2: score = me + 1
+    elif metal == 'GOLD':
+        gold = {0: ((them - 1) % 3) + 1,
+                1: them + 4,
+                2: ((them + 1) % 3) + 7}
+        score = gold[me]
     return score
-    
+ 
+def main():
+    with open('2_input.txt') as f:
+        data = f.readlines()
+    silver = sum([simulate(val, 'SILVER') for val in data])
+    gold = sum([simulate(val, 'GOLD') for val in data])
+    print(f'SILVER: {silver}')
+    print(f'GOLD: {gold}')
 
-with open('2_input.txt') as f:
-    vals = f.readlines()
-    total_score_silver = sum(map(parse_round_silver, vals))
-    total_score_gold = sum(map(parse_round_gold, vals))
-    print(total_score_silver)
-    print(total_score_gold)
+if __name__ == '__main__':
+    main()
