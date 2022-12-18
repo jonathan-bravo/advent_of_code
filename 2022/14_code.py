c79aca3151514e8aae68f3cc33fb1d25e226a1b1
Johnny Bravo <HaKmonkey@gmail.com>
2022-12-18 04:25:13 -0500

    Create 14_code.py
    scaffolding for the idea.. I think I want to animate this... for fun

---
+++ 2022/14_code.py
@@ -0,0 +1,15 @@
+# input is the vectors that make the rock walls
+## each line is a wall and wach vector is a line that is part of that wall
+
+# could make a class for the sand? Although that doesn't seem like it's worth
+## it unless I am tracking relation to other sand units
+
+
+class RockPath():
+    exit
+
+class Scan():
+    exit
+
+class Sand():
+    exit
