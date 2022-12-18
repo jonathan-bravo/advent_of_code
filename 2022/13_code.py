ec367b48368e94e9a7edf7c622fcb9869af8f821
Johnny Bravo <HaKmonkey@gmail.com>
2022-12-18 04:16:35 -0500

    Update 13_code.py
    Completed part 2 with a quicksort

--- 2022/13_code.py
+++ 2022/13_code.py
@@ -27,23 +27,30 @@ def com_pair(left, right):
     elif len(left) > len(right): return False
     else: return None
 
+def sort(l):
+    quicksort(l, 0, len(l) - 1)
+    return l
 
+def quicksort(l, head, tail):
+    if head < tail:
+        pivot = partition(l, head, tail)
+        quicksort(l, head, pivot - 1)
+        quicksort(l, pivot + 1, tail)
 
-
-
-
-# def flatten(container):
-#     for i in container:
-#         if isinstance(i, list):
-#             for j in flatten(i):
-#                 yield j
-#         else:
-#             yield i
+def partition(l, head, tail):
+    pivot = l[tail]
+    n = head
+    for i in range(head, tail):
+        if com_pair(eval(l[i]), eval(pivot)):
+            l[i], l[n] = l[n], l[i]
+            n = n + 1
+    l[n], l[tail] = l[tail], l[n]		
+    return n
 
 def main():
     with open('13_input.txt') as f:
         data = [line.strip() for line in f]
-
+        
     # PART 1
     left = data[::3]
     right = data[1::3]
@@ -60,10 +67,12 @@ def main():
     print(silver)
 
     # PART 2
-    left.append()
+    all_signals = [x for x in data if x != '']
+    all_signals.append('[[2]]')
+    all_signals.append('[[6]]')
+    sort(all_signals)
+    gold = (all_signals.index('[[2]]')+1) * (all_signals.index('[[6]]')+1)
+    print(gold)
 
 if __name__ == '__main__':
     main()
-
-
-
